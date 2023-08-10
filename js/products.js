b2c3aae68bb4f6fbad377aa70a34c9a20abec40f
agusgonzaro <agug2001@gmail.com>
2023-08-09 22:37:52 -0300

    ahora si

--- js/products.js
+++ js/products.js
@@ -1,21 +1,24 @@
-    const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const container = document.querySelector(".container1");
 
-    const container = document.getElementsByClassName("container"); 
-    
-    
-    onDOMContentLoaded = (event) => {function showData(dataArray) {
-      
-      for (const item of dataArray) {
-        container.innerHTML += `<p> ${item.cost} ${item.name} ${item.description}  ${item.soldCount}  ${item.image} </p>`;
-      }
+document.addEventListener("DOMContentLoaded", (event) => {
+  function showData(dataArray) {
+    for (const item of dataArray) {
+      container.innerHTML += `<p>${item.cost} ${item.name} ${item.description} ${item.soldCount} </p> <img src="${item.image}">`;
     }
+  }
 
-    fetch(DATA_URL)
-    .then((response)=>{
+  fetch(DATA_URL)
+    .then((response) => {
       if (response.ok) {
         return response.json();
       }
+      throw new Error("Network response was not ok.");
     })
     .then((data) => {
       showData(data.products);
-    })}
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+});
